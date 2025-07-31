00426db7229a57c6ea586f1c0a60e828f9d605a5
Olda Kruchna <okruchna@securityscorecard.io>
2025-07-31 11:23:22 +0200

    bundle local time js, add timestamp

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "local-time", integrity: "sha384-O5lr+l3FlQwSPyPZjAMu1oF63IQc2sIhmTO/okDLD275BuGpTCtreckeYgW19nWJ" # @3.0.3
